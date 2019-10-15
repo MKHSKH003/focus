a5ab229b33c8b736e1d3ccf3bd59b7cdc353aacf
MKHSKH003 <mkhskh003@myuct.ac.za>
2019-10-15 21:13:01 +0200

    Minor changes.

--- sql/generics/drop table column.sql
+++ sql/generics/drop table column.sql
@@ -1,16 +1,14 @@
 USE `-- DATABASE NAME --`;
 
 SET @table__name = '-- TABLE NAME --';
-SET @current_column_name = '-- CURRENT COLUMN NAME --';
-SET @new_column_name = '-- NEW COLUMN NAME --';
-SET @new_data_type = '-- DATA TYPE --';
+SET @column_name = '-- COLUMN NAME --';
 
 SELECT Count(*)
 INTO @exists
 FROM information_schema.COLUMNS 
-WHERE table_name = @table__name AND column_name = @current_column_name;
+WHERE table_name = @table__name AND column_name = @column_name;
 
-SET @alter_statment = concat('ALTER TABLE `',@table__name,'` CHANGE `',@current_column_name,'` `',@new_column_name,'` ',@new_data_type,'');
+SET @alter_statment = concat('ALTER TABLE ',@table__name,'` DROP COLUMN `',@column_name,'`');
 SET @query = If (@exists>0, @alter_statment, "SELECT ''");
 
 PREPARE stmt FROM @query;
